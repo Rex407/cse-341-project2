92727f3d35f76901404990fccbc3b358218e8b47
Rex407 <rexkapolom@outlook.com>
2026-03-20 20:23:11 +0200

    update db

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,24 @@
 const { MongoClient } = require("mongodb")
 require("dotenv").config()
 
-const client = new MongoClient(process.env.MONGODB_URI)
-
 let db
 
-async function connectDB() {
+const initDb = async () => {
+  if (db) return db
+
+  const client = new MongoClient(process.env.MONGODB_URI)
+  await client.connect()
+  db = client.db()
+
+  console.log("MongoDB connected")
+  return db
+}
+
+const getDb = () => {
   if (!db) {
-    await client.connect()
-    db = client.db()
-    console.log("MongoDB connected")
+    throw new Error("Database not initialized")
   }
   return db
 }
 
-module.exports = connectDB
+module.exports = { initDb, getDb }
