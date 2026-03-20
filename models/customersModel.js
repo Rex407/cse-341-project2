85976a33f34a54dc0ab655145e7321569eb10e37
Rex407 <rexkapolom@outlook.com>
2026-03-21 01:32:38 +0200

    Fix customers model DB usage

--- models/customersModel.js
+++ models/customersModel.js
@@ -1,31 +1,31 @@
-const connectDB = require("../db/connection")
+const { getDb } = require("../db/connection")
 const { ObjectId } = require("mongodb")
 
-async function getAllCustomers() {
-  const db = await connectDB()
+function getAllCustomers() {
+  const db = getDb()
   return db.collection("customers").find().toArray()
 }
 
-async function getCustomerById(id) {
-  const db = await connectDB()
+function getCustomerById(id) {
+  const db = getDb()
   return db.collection("customers").findOne({ _id: new ObjectId(id) })
 }
 
-async function createCustomer(customer) {
-  const db = await connectDB()
+function createCustomer(customer) {
+  const db = getDb()
   return db.collection("customers").insertOne(customer)
 }
 
-async function updateCustomer(id, customer) {
-  const db = await connectDB()
+function updateCustomer(id, customer) {
+  const db = getDb()
   return db.collection("customers").updateOne(
     { _id: new ObjectId(id) },
     { $set: customer }
   )
 }
 
-async function deleteCustomer(id) {
-  const db = await connectDB()
+function deleteCustomer(id) {
+  const db = getDb()
   return db.collection("customers").deleteOne({ _id: new ObjectId(id) })
 }
 
